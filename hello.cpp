6b92a5a8d7226928235ac4172bbaa457d77606c3
MMMeneghel <marinamantovanimeneghel@gmail.com>
2023-12-09 12:39:53 -0300

    bom dia

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+void print(){
+    std::cout << "Bom dia Mundo";
+}
+
 int main(){
 
-    std::cout << "Hello World!";
+    std::cout << "Hello World!\n";
+    print();
 
     return 0;
 }
